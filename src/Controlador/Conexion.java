f6ef32dfd6b73f24e887ab84de010ab47b4e33cb
MaggieSierra <43976301+MaggieSierra@users.noreply.github.com>
2020-11-23 12:14:18 -0600

    Conexion a la base de datos

---
+++ src/Controlador/Conexion.java
@@ -0,0 +1,31 @@
+package Controlador;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+	private static Connection connection;
+	
+	private Conexion() {
+		String db_url = "jdbc:mysql://db:3306/proyecto_tec";
+        String db_driver = "com.mysql.cj.jdbc.Driver";
+        String db_user = "root";
+        String db_password = "";
+        //String db_user = "root";
+        //String db_password = "root";
+        try {
+            Class.forName( db_driver );
+            connection = DriverManager.getConnection( db_url, db_user, db_password );
+        } catch( ClassNotFoundException | SQLException e ) {
+            e.printStackTrace();
+        }   
+	}
+	
+	public static Connection getConnection() {
+        if(  connection == null )
+            new Conexion();
+        return connection;
+    }
+	
+}
